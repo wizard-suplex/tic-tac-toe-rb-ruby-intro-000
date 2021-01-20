5522b4a2986e7ffe6ac50e371034735de9a86b96
Alan Cooke <atc4@rice.edu>
2021-01-20 19:43:25 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -36,6 +36,11 @@ def current_player(board)
   turn_count(board) % 2 == 0 ? "X" : "O"
 end
 
+# Make your move
+def move(board, index, player)
+  board[index] = player
+end
+
 # See if the move is valis
 def valid_move?(board, index)
   index.between?(0, 8) && !position_taken?(board, index)
